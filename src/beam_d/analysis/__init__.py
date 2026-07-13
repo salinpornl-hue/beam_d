c5169eefc393e8f193bd40342f2b63079c333056
salinpornl-hue <salinporn.l@gmail.com>
2026-07-13 11:20:12 +0700

    Add frame analysis foundation

---
+++ src/beam_d/analysis/__init__.py
@@ -0,0 +1,5 @@
+"""Analysis solvers and result objects."""
+
+from beam_d.analysis.frame2d import AnalysisResult, ElementAnalysisResult, FrameModel2D
+
+__all__ = ["AnalysisResult", "ElementAnalysisResult", "FrameModel2D"]
